6eee8590a982022e99aa5d9a6a536831d4c291ba
Hideto Noguchi <hidet9758@gmail.com>
2020-07-30 16:58:55 +0900

    deadline

--- Models/Task.php
+++ Models/Task.php
@@ -24,7 +24,7 @@ class Task extends Model
     public function update($data)
     {
         // データの更新
-        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET title = ?, contents = ? WHERE id = ? WHEN deadline = ?');
+        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET title = ?, contents = ? WHERE id = ? Time deadline = ?');
         $stmt->execute($data);
     }
 
