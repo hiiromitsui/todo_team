daf9e784a7c13764b4c7c65ce1c6a5d2902ce342
aoi shimazaki <fumizukinanoka@gmail.com>
2020-07-30 16:11:24 +0900

    deadlinephp

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+require_once('Models/Task.php');
+require_once('function.php');
+
+// ユーザーログイン
+session_start();
+if(isset($_SESSION['user'])){
+    $curretUser = $_SESSION['user'];
+    $email = $_SESSION['user']['email'];
+ }
+
+//  検索
+ $task = new Task();
+ $tasks = $task->getAll();
+?>
+
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -14,10 +32,17 @@
             <div class="col-12">
                 <nav class="navbar navbar-dark bg-dark">
                     <a href="index.php" class="navbar-brand">Todo_Team</a>
+
                     <ul class="nav nav-pills">
                         
                         <li class="nav-item">
-                            <a class="nav-link text-light"></a>
+                          <a class="nav-link text-light">
+                             <?php   
+                                if(isset($email)){
+                                    echo $email;
+                                } 
+                              ?>
+                          </a>
                         </li>
 
 
@@ -25,17 +50,22 @@
                             <a class="nav-link text-light" href="create.php">Create</a>
                         </li>
                       
-                        
-                                <a class="nav-link text-light" href="signout.php">Sign out</a>
-                        </li>
-                       
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="signupform.php">Sign up</a>
-                            </li>
+                            <?php 
+                            if(isset($curretUser)){
+                                echo '<li class="nav-item">
+                                        <a class="nav-link text-light" 
+                                        href="signout.php">Sign out</a></li>';
+                            } else {
+                                echo 
+                                '<li class="nav-item">
+                                    <a class="nav-link text-light" 
+                                    href="signupform.php">Sign up</a></li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="signinform.php">Sign in</a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" 
+                                    href="signinform.php">Sign in</a></li>';
+                                   }                        
+                            ?>
 
                         <li class="nav-item">
                             <form class="form-inline" method="get">
@@ -48,44 +78,46 @@
                 </nav>
             </div>
         </div>
-
+         <!-- ● -->
         <div class="row p-3">
-           
+           　　<?php foreach ($tasks as $task): ?>
                 <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
-                    <div class="card">
-                        <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"></h5>
-                            <p class="card-text">
-                                
-                            </p>
 
-                             <div class> 
-                                 <h2>Title</h2>
+                
+                             <div class="card"> 
+                             <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+
+                             <div class="card-body">
+                            <h2 class="card-title">
+                            <?= h($task["title"]); ?></h2>
+
+                           <h5> <p class="card-text">
+                                <?= h($task["contents"]); ?>
+                            </p><h/5>
+
+                            <p class="card-deadline">
+                                <?= h($task["deadline"]); ?>
+                            </p> 
+
+
+                                 <!-- <h2>Title</h2>
                                  <h5><p>Contents</p></h5><br>
                                  <p>Deadline 2020-07-29</p>
-        
+                             </div class> -->
                              
-                             </div class>
-                             
-
                             <div class="text-right d-flex justify-content-end">
 
-                                <a href="edit.php?id=" class="btn text-success">EDIT</a>
-                                
-
+                            <a href="edit.php?id=<?= h($task['id']); ?>" class="btn text-success">EDIT</a>
                                 <form action="delete.php" method="post">
-                                    <input type="hidden" name="id" value=" ">
+                                    <input type="hidden" name="id" value="<?= h($task['id']); ?>">
                                     <button type="submit" class="btn text-danger">DELETE</button>
-                               
-                                 </form>
-
+                                </form>
 
                             </div>
                         </div>
                     </div>
                 </div>
-            
+                <?php endforeach; ?>
         </div>
     </div>
 </body>
