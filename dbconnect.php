8ba20fa34e06a31817f1dfd1cd893dc45e951020
aoi shimazaki <fumizukinanoka@gmail.com>
2020-07-30 14:49:02 +0900

    dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -8,7 +8,6 @@ class DbManager
     {
         //DBに接続
         $host = "localhost";
-        // $dbname = "データベース名";
         $dbname = "todo_team";
         $charset = "utf8mb4";
         $user = 'root';
